96521267af70346890b24f9b45be2e726d8cb7f3
Parth Mudgal <artpar@gmail.com>
2017-06-13 16:30:12 +0530

    add dist files for docker and heroku
    Former-commit-id: e2c575e5cf5d5dd5ba56b8c4e34b9e111e7d7fa4

--- gomsweb/src/components/Sidebar.vue
+++ gomsweb/src/components/Sidebar.vue
@@ -26,22 +26,22 @@
       </div>
 
       <!-- search form (Optional) -->
-      <form v-on:submit.prevent class="sidebar-form">
-        <div class="input-group">
-          <input type="text"
-                 name="search"
-                 id="search"
-                 :value="filter"
-                 class="search form-control"
-                 data-toggle="hideseek" placeholder="Search Menus"
-                 data-list=".sidebar-menu">
-          <span class="input-group-btn">
-            <button type="submit" name="search" id="search-btn" class="btn btn-flat">
-              <i class="fa fa-search"></i>
-            </button>
-          </span>
-        </div>
-      </form>
+      <!--<form v-on:submit.prevent class="sidebar-form">-->
+        <!--<div class="input-group">-->
+          <!--<input type="text"-->
+                 <!--name="search"-->
+                 <!--id="search"-->
+                 <!--:value="filter"-->
+                 <!--class="search form-control"-->
+                 <!--data-toggle="hideseek" placeholder="Search Menus"-->
+                 <!--data-list=".sidebar-menu">-->
+          <!--<span class="input-group-btn">-->
+            <!--<button type="submit" name="search" id="search-btn" class="btn btn-flat">-->
+              <!--<i class="fa fa-search"></i>-->
+            <!--</button>-->
+          <!--</span>-->
+        <!--</div>-->
+      <!--</form>-->
       <!-- /.search form -->
 
       <!-- Sidebar Menu -->
