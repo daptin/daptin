af298cdb8c519ee2d2a7e6e0c697919b7ba86859
Parth Mudgal <artpar@gmail.com>
2017-06-13 16:19:00 +0530

    update build scripts to use gomsweb
    Former-commit-id: 3ae80df2edfc534bda448da783d5f0af3601bd48

--- gomsweb/src/filters/index.js
+++ gomsweb/src/filters/index.js
@@ -1,20 +1,19 @@
-const urlParser = document.createElement('a')
-
-export function domain (url) {
+export function domain(url) {
+  let urlParser = document.createElement('a')
   urlParser.href = url
   return urlParser.hostname
 }
 
-export function count (arr) {
+export function count(arr) {
   return arr.length
 }
 
-export function prettyDate (date) {
+export function prettyDate(date) {
   var a = new Date(date)
   return a.toDateString()
 }
 
-export function pluralize (time, label) {
+export function pluralize(time, label) {
   if (time === 1) {
     return time + label
   }
