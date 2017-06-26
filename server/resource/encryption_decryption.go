49735f386adea4288a874a9d0917dbbc67760240
Parth Mudgal <artpar@gmail.com>
2017-06-26 18:22:37 +0530

    vendor files for gsuite and gdrive. is the exe size going to explode ?
    Former-commit-id: 3733198e50d63a3f8c6bc10293acb8e93130c388

---
+++ server/resource/encryption_decryption.go
@@ -0,0 +1,77 @@
+package resource
+
+import (
+  "crypto/aes"
+  "crypto/cipher"
+  "crypto/rand"
+  "encoding/base64"
+  "fmt"
+  "io"
+  "github.com/pkg/errors"
+)
+
+func main() {
+  originalText := "Encrypt this golang"
+  fmt.Println(originalText)
+
+  key := []byte("example key 1234")
+
+  // Encrypt value to base64
+  cryptoText, _ := Encrypt(key, originalText)
+  fmt.Println(cryptoText)
+
+  // Encrypt base64 crypto to original value
+  text, _ := Decrypt(key, cryptoText)
+
+  fmt.Printf(text)
+}
+
+// Encrypt string to base64 crypto using AES
+func Encrypt(key []byte, text string) (string, error) {
+  // key := []byte(keyText)
+  plaintext := []byte(text)
+
+  block, err := aes.NewCipher(key)
+  if err != nil {
+    return "", err
+  }
+
+  // The IV needs to be unique, but not secure. Therefore it's common to
+  // include it at the beginning of the ciphertext.
+  ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+  iv := ciphertext[:aes.BlockSize]
+  if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+    return "", err
+  }
+
+  stream := cipher.NewCFBEncrypter(block, iv)
+  stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+  // convert to base64
+  return base64.URLEncoding.EncodeToString(ciphertext), nil
+}
+
+// Decrypt from base64 to decrypted string
+func Decrypt(key []byte, cryptoText string) (string, error) {
+  ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
+
+  block, err := aes.NewCipher(key)
+  if err != nil {
+    return "", err
+  }
+
+  // The IV needs to be unique, but not secure. Therefore it's common to
+  // include it at the beginning of the ciphertext.
+  if len(ciphertext) < aes.BlockSize {
+    return "", errors.New("Chipher text too short")
+  }
+  iv := ciphertext[:aes.BlockSize]
+  ciphertext = ciphertext[aes.BlockSize:]
+
+  stream := cipher.NewCFBDecrypter(block, iv)
+
+  // XORKeyStream can work in-place if the two arguments are the same.
+  stream.XORKeyStream(ciphertext, ciphertext)
+
+  return fmt.Sprintf("%s", ciphertext), nil
+}
---
+++ server/resource/exchange_drive.go
@@ -0,0 +1,68 @@
+package resource
+
+import (
+  "github.com/artpar/api2go"
+  "google.golang.org/api/drive/v3"
+  log "github.com/sirupsen/logrus"
+  "context"
+  "golang.org/x/oauth2"
+)
+
+type GdriveExternalExchange struct {
+  token      *oauth2.Token
+  columnInfo []api2go.ColumnInfo
+  config     *oauth2.Config
+}
+
+func (g *GdriveExternalExchange) UpdateDestination(destinationName string, data []map[string]interface{}) (error) {
+
+  return nil
+}
+func (g *GdriveExternalExchange) ReadDestination(destinationName string) ([]map[string]interface{}, error) {
+
+  ctx := context.Background()
+  client := g.config.Client(ctx, g.token)
+
+  srv, err := drive.New(client)
+
+  if err != nil {
+    log.Fatalf("Unable to retrieve Gdrive Client %v", err)
+  }
+
+  filesList, err := srv.Files.List().Do()
+
+  if err != nil {
+    return nil, err
+  }
+
+  resp := make([]map[string]interface{}, 0)
+
+  for _, file := range filesList.Files {
+
+    row := make(map[string]interface{})
+
+    row["name"] = file.Name
+
+    for k, v := range file.AppProperties {
+      row[k] = v
+    }
+
+    row["created_at"] = file.CreatedTime
+    row["description"] = file.Description
+    row["file_extension"] = file.FileExtension
+    row["folder_color_rgs"] = file.FolderColorRgb
+    row["reference_id"] = file.Id
+
+    resp = append(resp, row)
+  }
+
+  return resp, nil
+}
+
+func NewGdriveExternalExchange(columnInfo []api2go.ColumnInfo, token *oauth2.Token) ExternalExchange {
+
+  return &GdriveExternalExchange{
+    token:      token,
+    columnInfo: columnInfo,
+  }
+}
