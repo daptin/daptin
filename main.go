73ba4367136bb32fab2e0e8a1f8c8cc7e9241909
Parth Mudgal <artpar@gmail.com>
2017-07-04 16:37:46 +0530

    move find rice box to root to avoid relative paths
    Former-commit-id: 2a5d558a677684f48fd864919854719496e2de6f

--- main.go
+++ main.go
@@ -1,108 +1,112 @@
 package main
 
 import (
-	"github.com/artpar/goms/server"
-	"github.com/rcrowley/goagain"
-	"log"
-	//"os"
-	"fmt"
-	"net"
-	"sync"
-	"syscall"
-	"time"
+  "github.com/artpar/goms/server"
+  "github.com/rcrowley/goagain"
+  "log"
+  //"os"
+  "fmt"
+  "net"
+  "sync"
+  "syscall"
+  "time"
+  "github.com/GeertJohan/go.rice"
 )
 
 func init() {
-	goagain.Strategy = goagain.Double
-	log.SetFlags(log.Lmicroseconds | log.Lshortfile)
-	log.SetPrefix(fmt.Sprintf("pid:%d ", syscall.Getpid()))
+  goagain.Strategy = goagain.Double
+  log.SetFlags(log.Lmicroseconds | log.Lshortfile)
+  log.SetPrefix(fmt.Sprintf("pid:%d ", syscall.Getpid()))
 }
 
 func main() {
 
-	// Inherit a net.Listener from our parent process or listen anew.
-	ch := make(chan struct{})
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	l, err := goagain.Listener()
-	if nil != err {
+  boxStatic := rice.MustFindBox("gomsweb/dist/static").HTTPBox()
+  boxRoot := rice.MustFindBox("gomsweb/dist").HTTPBox()
 
-		// Listen on a TCP or a UNIX domain socket (TCP here).
-		l, err = net.Listen("tcp", "127.0.0.1:48879")
-		if nil != err {
-			log.Fatalln(err)
-		}
-		log.Println("listening on", l.Addr())
+  // Inherit a net.Listener from our parent process or listen anew.
+  ch := make(chan struct{})
+  wg := &sync.WaitGroup{}
+  wg.Add(1)
+  l, err := goagain.Listener()
+  if nil != err {
 
-		// Accept connections in a new goroutine.
-		go server.Main()
-		go serve(l, ch, wg)
+    // Listen on a TCP or a UNIX domain socket (TCP here).
+    l, err = net.Listen("tcp", "127.0.0.1:48879")
+    if nil != err {
+      log.Fatalln(err)
+    }
+    log.Println("listening on", l.Addr())
 
-	} else {
+    // Accept connections in a new goroutine.
+    go server.Main(boxRoot, boxStatic)
+    go serve(l, ch, wg)
 
-		// Resume listening and accepting connections in a new goroutine.
-		log.Println("resuming listening on", l.Addr())
-		go server.Main()
-		go serve(l, ch, wg)
+  } else {
 
-		// If this is the child, send the parent SIGUSR2.  If this is the
-		// parent, send the child SIGQUIT.
-		if err := goagain.Kill(); nil != err {
-			log.Fatalln(err)
-		}
+    // Resume listening and accepting connections in a new goroutine.
+    log.Println("resuming listening on", l.Addr())
+    go server.Main(boxRoot, boxStatic)
+    go serve(l, ch, wg)
 
-	}
+    // If this is the child, send the parent SIGUSR2.  If this is the
+    // parent, send the child SIGQUIT.
+    if err := goagain.Kill(); nil != err {
+      log.Fatalln(err)
+    }
 
-	// Block the main goroutine awaiting signals.
-	sig, err := goagain.Wait(l)
-	if nil != err {
-		log.Fatalln(err)
-	}
+  }
 
-	// Do whatever's necessary to ensure a graceful exit like waiting for
-	// goroutines to terminate or a channel to become closed.
-	//
-	// In this case, we'll close the channel to signal the goroutine to stop
-	// accepting connections and wait for the goroutine to exit.
-	close(ch)
-	wg.Wait()
+  // Block the main goroutine awaiting signals.
+  sig, err := goagain.Wait(l)
+  if nil != err {
+    log.Fatalln(err)
+  }
 
-	// If we received SIGUSR2, re-exec the parent process.
-	if goagain.SIGUSR2 == sig {
-		if err := goagain.Exec(l); nil != err {
-			log.Fatalln(err)
-		}
-	}
+  // Do whatever's necessary to ensure a graceful exit like waiting for
+  // goroutines to terminate or a channel to become closed.
+  //
+  // In this case, we'll close the channel to signal the goroutine to stop
+  // accepting connections and wait for the goroutine to exit.
+  close(ch)
+  wg.Wait()
+
+  // If we received SIGUSR2, re-exec the parent process.
+  if goagain.SIGUSR2 == sig {
+    if err := goagain.Exec(l); nil != err {
+      log.Fatalln(err)
+    }
+  }
 }
 
 // A very rude server that says hello and then closes your connection.
 func serve(l net.Listener, ch chan struct{}, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for {
+  defer wg.Done()
+  for {
 
-		// Break out of the accept loop on the next iteration after the
-		// process was signaled and our channel was closed.
-		select {
-		case <-ch:
-			return
-		default:
-		}
+    // Break out of the accept loop on the next iteration after the
+    // process was signaled and our channel was closed.
+    select {
+    case <-ch:
+      return
+    default:
+    }
 
-		// Set a deadline so Accept doesn't block forever, which gives
-		// us an opportunity to stop gracefully.
-		l.(*net.TCPListener).SetDeadline(time.Now().Add(100e6))
+    // Set a deadline so Accept doesn't block forever, which gives
+    // us an opportunity to stop gracefully.
+    l.(*net.TCPListener).SetDeadline(time.Now().Add(100e6))
 
-		c, err := l.Accept()
-		if nil != err {
-			if goagain.IsErrClosing(err) {
-				return
-			}
-			if err.(*net.OpError).Timeout() {
-				continue
-			}
-			log.Fatalln(err)
-		}
-		c.Write([]byte("Hello, world!\n"))
-		c.Close()
-	}
+    c, err := l.Accept()
+    if nil != err {
+      if goagain.IsErrClosing(err) {
+        return
+      }
+      if err.(*net.OpError).Timeout() {
+        continue
+      }
+      log.Fatalln(err)
+    }
+    c.Write([]byte("Hello, world!\n"))
+    c.Close()
+  }
 }
