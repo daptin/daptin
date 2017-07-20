ce647e7626288e4fbcf1e2cbfbea208a6a5e0107
Parth Mudgal <artpar@gmail.com>
2017-07-20 22:06:21 +0530

    update view files, show permission in model form
    Former-commit-id: 27e178e97daf4551310b00740f30048303a976dc [formerly 70f4f7f3955cb0d51e7316a93f326d0c8e50b868]
    Former-commit-id: 0328fe15e7e8abe393fcc6f70fbddb760c70d635

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash
-
 
 rm -rf docker_dir
 mkdir docker_dir
 
 cp main docker_dir/main
+cp -Rf gomsweb/dist docker_dir/static
 
 cp Dockerfile docker_dir/Dockerfile
 
--- scripts/build_server.sh
+++ scripts/build_server.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+
 
 export GOPATH=/media/artpar/ddrive/workspace/newgocode
 rice embed-go
