1e68dbf4f43a6176ecd8490ce28e1fbe6b030e99
artpar <artpar@gmail.com>
2017-12-28 16:25:21 +0530

    - Image uploads to be stored in cloud store - Image upload metadata stored in entity columns - Can be multiple files - Handle file on ui using relations - Allow multiple type of editors for json/text in entities.
    Former-commit-id: bccc146221b6d8ace7ac8ec6954688b3208b4ee1 [formerly 61a1be63abb59b2b67d22ae5eb90bb39ee5022f5] [formerly 9eb9dcce7ffc3a5e50f04fb718a835bb8da2145b [formerly 1f517de8f12190510dad4ab882d345a7b31d2272]] [formerly bccc146221b6d8ace7ac8ec6954688b3208b4ee1 [formerly 61a1be63abb59b2b67d22ae5eb90bb39ee5022f5] [formerly 9eb9dcce7ffc3a5e50f04fb718a835bb8da2145b [formerly 1f517de8f12190510dad4ab882d345a7b31d2272]] [formerly 5172f9ea82b1bcf56d90cf3c2df01b51920dbecc]]
    Former-commit-id: 9edf1f41e5efe0b8913d2a8a7448d527e6e445af
    Former-commit-id: 87c562ecb4a66b28f89790089c551476d720fc33
    Former-commit-id: dd0fee1c2f33edfe23ca681b93d36257921b3c91 [formerly 0542646b4ba56e53ad8b93c05db2a0e7d5e3fc2f]
    Former-commit-id: 4b4ed899a34c23206af6a2e7ca72ce32c3628b0c

--- server/resource/resource_findone.go
+++ server/resource/resource_findone.go
@@ -62,10 +62,14 @@ func (dr *DbResource) FindOne(referenceId string, req api2go.Request) (api2go.Re
 		p, ok := inc["permission"].(int64)
 		if !ok {
 			log.Errorf("Failed to convert [%v] to permission: %v", inc["permission"], ok)
-			continue
+			p = 0
 		}
 		incType := inc["__type"].(string)
-		a.Includes = append(a.Includes, api2go.NewApi2GoModelWithData(incType, dr.cruds[incType].model.GetColumns(), int64(p), dr.cruds[incType].model.GetRelations(), inc))
+		if BeginsWith(incType, "image.") {
+			a.Includes = append(a.Includes, api2go.NewApi2GoModelWithData(incType, nil, 0, nil, inc))
+		} else {
+			a.Includes = append(a.Includes, api2go.NewApi2GoModelWithData(incType, dr.cruds[incType].model.GetColumns(), int64(p), dr.cruds[incType].model.GetRelations(), inc))
+		}
 	}
 
 	return NewResponse(nil, a, 200, nil), err
