112ab047f962ea5f8a271b3fbbe0dffa53f293a7
artpar <artpar@gmail.com>
2018-01-15 23:32:52 +0530

    add automated cross compilation build script
    Former-commit-id: c306920b6fca698b1e2a677335e581e846608214 [formerly a8505dbe89b95d7cc22eaa651b8865b63c154bd5] [formerly affe3598e25aaa589f7809fcc7b1cd4ded9c8810 [formerly 0a6d378b450585e53522f10dc92043aa1ef6fcda]] [formerly c306920b6fca698b1e2a677335e581e846608214 [formerly a8505dbe89b95d7cc22eaa651b8865b63c154bd5] [formerly affe3598e25aaa589f7809fcc7b1cd4ded9c8810 [formerly 0a6d378b450585e53522f10dc92043aa1ef6fcda]] [formerly 017f4d5b2e3688441a11c5bbbf1b3673ecbd7103]]
    Former-commit-id: b4d68660cd45c4e3ac54cd1b459efecae50a40f1
    Former-commit-id: 81da6d05d4b877c36fb1ed8a007a11f7ab37d7e3
    Former-commit-id: ceb6af7b5991ad798831bed58aa204d271748ba5 [formerly 00f9dd4c9b5332b1c181eab5b3e3983a6c7edbac]
    Former-commit-id: ccb30db82cb81f0e4bff4d1adcd31516664ee6d2

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,14 +1,23 @@
+#!/usr/bin/env bash
+
+
+cd daptinweb
+# npm run build
+cd ..
+go get github.com/GeertJohan/go.rice
+go get github.com/GeertJohan/go.rice/rice
+curl https://glide.sh/get | sh
+echo "start go get"
+glide install
+echo "finish go get"
+rm -rf rice-box.go
+rice embed-go
+CGO_ENABLED=1
+
+go build  -ldflags '-linkmode external -extldflags -static -w' main.go
+rice append --exec main
 
 rm -rf docker_dir
 mkdir docker_dir
 
-cp main docker_dir/main
-cp -Rf daptinweb/dist docker_dir/static
 
-cp Dockerfile docker_dir/Dockerfile
-
-cd docker_dir
-docker build -t daptin/daptin  .
-
-cd ..
-docker images | grep daptin | grep latest
