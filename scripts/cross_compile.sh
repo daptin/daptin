3a3ce04cd87e2259c403f3d5cfac513888b97d4c
artpar <artpar@gmail.com>
2018-01-16 00:44:19 +0530

    update cross build scripts
    Former-commit-id: 29b49f34c29374f9288252c56e3f66e2db3c30dd
    Former-commit-id: 6c9fbd7a1b3aae5dc6ed23b9c2de99383ef669bb [formerly 6805e03c5bcd3be09ed9d923076c4259e5841a59]
    Former-commit-id: dd75ae5ac25c27a831a8ac09728c62b2ac9bb0e2

--- scripts/cross_compile.sh
+++ scripts/cross_compile.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -v "$GOPATH":/go -e GOOS=windows -e GOARCH=386 -w /go/src/github.com/daptin/daptin golang-crosscompile  ./scripts/build_docker.sh
-docker run --rm -it -v "$GOPATH":/go -e GOOS=linux -e GOARCH=amd64 -w /go/src/github.com/daptin/daptin golang-crosscompile  ./scripts/build_docker.sh
-docker run --rm -it -v "$GOPATH":/go -e GOOS=linux -e GOARCH=arm -e GOARM=6 -w /go/src/github.com/daptin/daptin golang-crosscompile  ./scripts/build_docker.sh
+docker run --rm -it -v "$GOPATH":/go -e "GOARCH=amd64" -w /go/src/github.com/daptin/daptin golang-crosscompile  ./scripts/build_docker.sh
+docker run --rm -it -v "$GOPATH":/go -e GOARCH="386" -e CC="gcc -m32" -w /go/src/github.com/daptin/daptin golang-crosscompile  ./scripts/build_docker.sh
+#docker run --rm -it -v "$GOPATH":/go -e GOOS=windows -e GOARCH=amd64 -w /go/src/github.com/daptin/daptin golang-crosscompile  ./scripts/build_docker.sh
+#docker run --rm -it -v "$GOPATH":/go -e GOOS=linux -e GOARCH=amd64 -w /go/src/github.com/daptin/daptin golang-crosscompile  ./scripts/build_docker.sh
+#docker run --rm -it -v "$GOPATH":/go -e GOOS=linux -e GOARCH=arm -e GOARM=6 -w /go/src/github.com/daptin/daptin golang-crosscompile  ./scripts/build_docker.sh
+#docker run --rm -it -v "$GOPATH":/go -e GOOS=darwin -e GOARCH=amd64 -w /go/src/github.com/daptin/daptin golang-crosscompile  ./scripts/build_docker.sh
