762392b84b33df5428bfce008629449124563c1e
artpar <artpar@gmail.com>
2018-06-20 21:10:53 +0530

    - a new action, "create response" to structure the response of actions after all outcomes

---
+++ server/resource/action_make_response.go
@@ -0,0 +1,38 @@
+package resource
+
+import (
+	"github.com/artpar/api2go"
+)
+
+/**
+  Become administrator of daptin action implementation
+*/
+type MakeResponsePerformer struct {
+}
+
+// Name of the action
+func (d *MakeResponsePerformer) Name() string {
+	return "response.create"
+}
+
+// Perform action and try to make the current user the admin of the system
+// Checks CanBecomeAdmin and then invokes BecomeAdmin if true
+func (d *MakeResponsePerformer) DoAction(request ActionRequest, inFieldMap map[string]interface{}) (api2go.Responder, []ActionResponse, []error) {
+	responseType, ok := inFieldMap["response_type"]
+	if !ok {
+		responseType = request.Type
+	}
+	return nil, []ActionResponse{
+		NewActionResponse(responseType.(string), inFieldMap),
+	}, nil
+}
+
+// Create a new action performer for becoming administrator action
+func NewMakeResponsePerformer() (ActionPerformerInterface, error) {
+
+	handler := MakeResponsePerformer{
+	}
+
+	return &handler, nil
+
+}
