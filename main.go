205b1e63ae0ea9849533030e12f6f7a8d9a87af4
Parth Mudgal <artpar@gmail.com>
2020-02-18 19:41:50 +0530

    close imap server on restart

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"github.com/GeertJohan/go.rice"
 	"github.com/artpar/go-guerrilla"
+	imapServer "github.com/artpar/go-imap/server"
 	"github.com/daptin/daptin/server"
 	"github.com/daptin/daptin/server/resource"
 	"github.com/daptin/daptin/server/statementbuilder"
@@ -87,8 +88,9 @@ func main() {
 	var taskScheduler resource.TaskScheduler
 	var certManager *resource.CertificateManager
 	var configStore *resource.ConfigStore
+	var imapServer *imapServer.Server
 
-	hostSwitch, mailDaemon, taskScheduler, configStore, certManager = server.Main(boxRoot, db)
+	hostSwitch, mailDaemon, taskScheduler, configStore, certManager, imapServer = server.Main(boxRoot, db)
 	rhs := RestartHandlerServer{
 		HostSwitch: &hostSwitch,
 	}
@@ -98,6 +100,10 @@ func main() {
 
 		taskScheduler.StopTasks()
 		mailDaemon.Shutdown()
+		err = imapServer.Close()
+		if err != nil {
+			log.Printf("Failed to close DB connections: %v", err)
+		}
 		err = db.Close()
 		if err != nil {
 			log.Printf("Failed to close DB connections: %v", err)
@@ -105,7 +111,7 @@ func main() {
 
 		db, err = server.GetDbConnection(*db_type, *connection_string)
 
-		hostSwitch, mailDaemon, taskScheduler, configStore, certManager = server.Main(boxRoot, db)
+		hostSwitch, mailDaemon, taskScheduler, configStore, certManager, imapServer = server.Main(boxRoot, db)
 		rhs.HostSwitch = &hostSwitch
 		log.Printf("Restart complete")
 	})
