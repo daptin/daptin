f03b2b6aa4a4abc49fcddc1107be940cb2e2dd32
Parth Mudgal <artpar@gmail.com>
2020-03-10 22:30:24 +0530

    update xlsx dependency to v2

--- server/resource/dbfunctions_update.go
+++ server/resource/dbfunctions_update.go
@@ -15,7 +15,7 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
-	"github.com/tealeg/xlsx"
+	"github.com/tealeg/xlsx/v2"
 	"io/ioutil"
 	"net/http"
 	"os"
