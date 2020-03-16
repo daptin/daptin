25b2ed007afc36ddb807cf8f699f1b977c1f91bd
Parth Mudgal <artpar@gmail.com>
2020-03-16 20:56:03 +0530

    - sleep before next start, allow storage sync to complete before test proceeds

--- server_test.go
+++ server_test.go
@@ -544,11 +544,11 @@ func RunTests(t *testing.T, hostSwitch server.HostSwitch, daemon *guerrilla.Daem
 	//	return err
 	//}
 
-	time.Sleep(5 * time.Second)
+	time.Sleep(8 * time.Second)
 	trigger.Fire("restart")
 
 	t.Logf("Sleeping for 5 seconds waiting for restart")
-	time.Sleep(5 * time.Second)
+	time.Sleep(8 * time.Second)
 	t.Logf("Wake up after sleep")
 
 	resp, err = r.Get(baseAddress+"/_config/backend/hostname", authTokenHeader)
