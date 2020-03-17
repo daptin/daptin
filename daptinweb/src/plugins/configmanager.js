0ce4329ab0527c9375603df62f7887ae913212c8
Parth Mudgal <artpar@gmail.com>
2020-03-17 20:10:40 +0530

    - a working configuration editing page - update docs

--- daptinweb/src/plugins/configmanager.js
+++ daptinweb/src/plugins/configmanager.js
@@ -4,15 +4,53 @@
 import appconfig from "./appconfig"
 
 import axios from "axios"
+import {getToken} from "../utils/auth"
 
 const ConfigManager = function () {
 
 
+  this.getConfig = function (name) {
+    return new Promise(function (resolve, reject) {
+      axios({
+        url: appconfig.apiRoot + "/_config/backend/" + name,
+        method: "GET",
+        headers: {
+          "Authorization": "Bearer " + getToken(),
+        },
+      }).then(function (r) {
+        resolve(r.data)
+      }, function (r) {
+        reject(r);
+      });
+    })
+  };
+
+
+  this.setConfig = function (name, value) {
+    return new Promise(function (resolve, reject) {
+      axios({
+        url: appconfig.apiRoot + "/_config/backend/" + name,
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + getToken(),
+        },
+        data: value
+      }).then(function (r) {
+        resolve(r.data)
+      }, function (r) {
+        reject(r);
+      });
+    })
+  };
+
   this.getAllConfig = function () {
     var p = new Promise(function (resolve, reject) {
       axios({
-        url: appconfig.apiRoot + "/config",
+        url: appconfig.apiRoot + "/_config",
         method: "GET",
+        headers: {
+          "Authorization": "Bearer " + getToken(),
+        },
       }).then(function (r) {
         resolve(r.data)
       }, function (r) {
