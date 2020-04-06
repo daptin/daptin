4178e5fb4069224afed3664afd4deaa460f2cd07
artpar <artpar@gmail.com>
2020-04-06 12:23:24 +0530

    - handle "undefined" in Accept-Language #83

--- server/resource/resource_findallpaginated.go
+++ server/resource/resource_findallpaginated.go
@@ -496,7 +496,7 @@ func (dr *DbResource) PaginatedFindAllWithoutFilters(req api2go.Request) ([]map[
 		return nil, nil, nil, err
 	}
 	log.Printf("Id query: [%s]", idsListQuery)
-	log.Printf("Id query args: %v", args)
+	//log.Printf("Id query args: %v", args)
 	stmt, err := dr.connection.Preparex(idsListQuery)
 	if err != nil {
 		log.Infof("Findall select query sql: %v == %v", idsListQuery, args)
