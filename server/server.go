ba279ddaafe760a86f9ae8ba53932f484755a7fc
Parth Mudgal <artpar@gmial.com>
2021-01-19 19:02:26 +0530

    - build docker image on ubuntu

--- server/server.go
+++ server/server.go
@@ -147,6 +147,7 @@ func Main(boxRoot http.FileSystem, db database.DatabaseConnection, localStorageP
 
 	defaultRouter.Use(NewCorsMiddleware().CorsMiddlewareFunc)
 	defaultRouter.StaticFS("/static", NewSubPathFs(boxRoot, "/static"))
+	defaultRouter.StaticFS("/statics", NewSubPathFs(boxRoot, "/statics"))
 	defaultRouter.StaticFS("/js", NewSubPathFs(boxRoot, "/js"))
 	defaultRouter.StaticFS("/css", NewSubPathFs(boxRoot, "/css"))
 	defaultRouter.StaticFS("/fonts", NewSubPathFs(boxRoot, "/fonts"))
