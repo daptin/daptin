779d9418142ef856a9a84f3b02674245c0558135
Parth Mudgal <artpar@gmial.com>
2021-07-14 16:15:58 +0530

    set max/idle db connections and lifetime from env config

--- server/database_connection.go
+++ server/database_connection.go
@@ -5,6 +5,9 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"strconv"
 	"strings"
 	"time"
 	//"github.com/casbin/xorm-adapter"
@@ -30,13 +33,66 @@ func GetDbConnection(dbType string, connectionString string) (*sqlx.DB, error) {
 	}
 
 	db, e := sqlx.Open(dbType, connectionString)
+
 	if e != nil {
 		return nil, e
 	}
-	db.SetMaxIdleConns(10)
-	db.SetMaxOpenConns(50)
-	db.SetConnMaxLifetime(5 * time.Minute)
-	db.SetConnMaxIdleTime(1 * time.Minute)
+
+	maxIdleConnections := os.Getenv("DAPTIN_MAX_IDLE_CONNECTIONS")
+	if maxIdleConnections == "" {
+		maxIdleConnections = "10"
+	}
+	maxOpenConnections := os.Getenv("DAPTIN_MAX_OPEN_CONNECTIONS")
+	if maxOpenConnections == "" {
+		maxOpenConnections = "50"
+	}
+	maxConnectionLifetimeMinString := os.Getenv("DAPTIN_MAX_CONNECTIONS_LIFETIME")
+	if maxConnectionLifetimeMinString == "" {
+		maxConnectionLifetimeMinString = "5"
+	}
+	maxConnectionIdleTimeMinString := os.Getenv("DAPTIN_MAX_IDLE_CONNECTIONS_TIME")
+	if maxConnectionIdleTimeMinString == "" {
+		maxConnectionIdleTimeMinString = "1"
+	}
+
+	maxIdleConnectionsInt, err := strconv.ParseInt(maxIdleConnections, 10, 64)
+	if err != nil {
+		maxIdleConnectionsInt = 10
+	}
+
+	maxOpenConnectionsInt, err := strconv.ParseInt(maxOpenConnections, 10, 64)
+	if err != nil {
+		maxOpenConnectionsInt = 50
+	}
+
+	maxConnectionLifetimeMinInt, err := strconv.ParseInt(maxConnectionLifetimeMinString, 10, 64)
+	if err != nil {
+		maxConnectionLifetimeMinInt = 5
+	}
+
+	maxConnectionIdleTimeMinInt, err := strconv.ParseInt(maxConnectionIdleTimeMinString, 10, 64)
+	if err != nil {
+		maxConnectionIdleTimeMinInt = 5
+	}
+
+	maxConnectionLifetimeMin := time.Duration(maxConnectionLifetimeMinInt) * time.Minute
+	maxConnectionIdleTimeMin := time.Duration(maxConnectionIdleTimeMinInt) * time.Minute
+
+	db.SetMaxIdleConns(int(maxIdleConnectionsInt))
+	db.SetMaxOpenConns(int(maxOpenConnectionsInt))
+
+	db.SetConnMaxLifetime(maxConnectionLifetimeMin)
+	db.SetConnMaxIdleTime(maxConnectionIdleTimeMin)
+	log.Infof("Database Connection Params: " +
+		"Max Idle Connections: [%v], " +
+		"Max Open Connections: [%v] , " +
+		"Max Connection Life time: [%v] , " +
+		"Max Idle Connection life time: [%v] ",
+		maxIdleConnectionsInt,
+		maxOpenConnectionsInt,
+		maxConnectionLifetimeMin,
+		maxConnectionIdleTimeMin)
+
 	return db, e
 }
 
