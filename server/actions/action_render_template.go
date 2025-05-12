d0860cb59f9d477a40ad6cb4ab0665585964439e
Parth Mudgal <artpar@gmail.com>
2025-05-12 16:05:09 +0530

    template added new syntax to pick file from subsite subsite://<reference-id>/index.html

--- server/actions/action_render_template.go
+++ server/actions/action_render_template.go
@@ -4,11 +4,15 @@ import (
 	"errors"
 	"github.com/artpar/api2go"
 	"github.com/daptin/daptin/server/actionresponse"
+	"github.com/daptin/daptin/server/assetcachepojo"
+	daptinid "github.com/daptin/daptin/server/id"
 	"github.com/daptin/daptin/server/resource"
 	"github.com/flysnow-org/soha"
 	"github.com/jmoiron/sqlx"
 	log "github.com/sirupsen/logrus"
 	"html/template"
+	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -53,6 +57,39 @@ func (actionPerformer *renderTemplateActionPerformer) DoAction(
 		return nil, []actionresponse.ActionResponse{}, []error{errors.New("no template content found")}
 	}
 
+	// Check if the template content is a reference to a subsite file
+	if strings.HasPrefix(templateContent, "subsite://") {
+		// Parse the subsite path: "subsite://<site_reference_id>/path/to/file"
+		pathParts := strings.SplitN(strings.TrimPrefix(templateContent, "subsite://"), "/", 2)
+		if len(pathParts) != 2 {
+			return nil, []actionresponse.ActionResponse{}, []error{errors.New("invalid subsite file path format, expected: subsite://<site_reference_id>/path/to/file")}
+		}
+
+		siteReferenceIdStr := pathParts[0]
+		filePath := pathParts[1]
+
+		// Convert site reference ID string to DaptinReferenceId
+		siteReferenceId := daptinid.InterfaceToDIR(siteReferenceIdStr)
+		if siteReferenceId == daptinid.NullReferenceId {
+			return nil, []actionresponse.ActionResponse{}, []error{errors.New("invalid site reference ID: " + siteReferenceIdStr)}
+		}
+
+		// Get the subsite folder cache
+		assetFolderCache, ok := actionPerformer.cruds["template"].SubsiteFolderCache(siteReferenceId)
+		if !ok {
+			return nil, []actionresponse.ActionResponse{}, []error{errors.New("subsite not found for reference ID: " + siteReferenceIdStr)}
+		}
+
+		// Load the file content from the subsite
+		fileContent, err := loadFileFromSubsite(assetFolderCache, filePath)
+		if err != nil {
+			return nil, []actionresponse.ActionResponse{}, []error{errors.New("failed to load file from subsite: " + err.Error())}
+		}
+
+		// Use the file content as the template content
+		templateContent = fileContent
+	}
+
 	sohaFuncMap := soha.CreateFuncMap()
 
 	tmpl, err := template.New(template_name).Funcs(sohaFuncMap).Parse(templateContent)
@@ -79,6 +116,31 @@ func (actionPerformer *renderTemplateActionPerformer) DoAction(
 	resp.Res = responder
 
 	return resp, []actionresponse.ActionResponse{}, nil
+}
+
+// loadFileFromSubsite loads a file from a subsite's local sync path
+func loadFileFromSubsite(assetFolderCache *assetcachepojo.AssetFolderCache, filePath string) (string, error) {
+	// Construct the full path to the file
+	fullPath := assetFolderCache.LocalSyncPath + string(os.PathSeparator) + filePath
+
+	// Check if the file exists
+	fileInfo, err := os.Stat(fullPath)
+	if err != nil {
+		return "", err
+	}
+
+	// Check if it's a regular file
+	if fileInfo.IsDir() {
+		return "", errors.New("path is a directory, not a file")
+	}
+
+	// Read the file content
+	fileBytes, err := ioutil.ReadFile(fullPath)
+	if err != nil {
+		return "", err
+	}
+
+	return string(fileBytes), nil
 }
 
 func NewRenderTemplateActionPerformer(cruds map[string]*resource.DbResource, configStore *resource.ConfigStore, transaction *sqlx.Tx) (actionresponse.ActionPerformerInterface, error) {
